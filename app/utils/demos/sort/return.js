f9866a03ecede989ef4923928bd525cab93c7efc
christian-kesler-2022 <christian.kesler.2022@gmail.com>
2022-08-29 15:20:04 -0700

    Add sample files

--- app/utils/demos/sort/return.js
+++ app/utils/demos/sort/return.js
@@ -1,13 +1,11 @@
 var fs = require('fs');
 
 fs.readdir(__dirname + '/output/pass/', function (err, files) {
-  //handling error
   if (err) {
     return console.log('Unable to scan directory: ' + err);
   }
-  //listing all files using forEach
+
   files.forEach(function (file) {
-    // Do whatever you want to do with the file
     fs.rename(
       __dirname + '/output/pass/' + file,
       __dirname + '/input/' + file,
@@ -16,17 +14,24 @@ fs.readdir(__dirname + '/output/pass/', function (err, files) {
         console.log('Successfully moved ' + file);
       }
     );
+    console.log(
+      __dirname +
+        '/output/pass/' +
+        file +
+        ' --> ' +
+        __dirname +
+        '/input/' +
+        file
+    );
   });
 });
 
 fs.readdir(__dirname + '/output/fail/', function (err, files) {
-  //handling error
   if (err) {
     return console.log('Unable to scan directory: ' + err);
   }
-  //listing all files using forEach
+
   files.forEach(function (file) {
-    // Do whatever you want to do with the file
     fs.rename(
       __dirname + '/output/fail/' + file,
       __dirname + '/input/' + file,
@@ -34,6 +39,15 @@ fs.readdir(__dirname + '/output/fail/', function (err, files) {
         if (err) throw err;
         console.log('Successfully moved ' + file);
       }
+    );
+    console.log(
+      __dirname +
+        '/output/pass/' +
+        file +
+        ' --> ' +
+        __dirname +
+        '/input/' +
+        file
     );
   });
 });
