ae987842043c8d012d7e676603fb4960259f66eb
christian-kesler-2022 <christian.kesler.2022@gmail.com>
2022-08-29 08:46:15 -0700

    Create crude page splicing functionality

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-// run `node index.js` in the terminal
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function (req, res) {
+  console.log('requested url: ' + req.url);
+
+  if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+
+    var header = fs.readFileSync(
+      __dirname + '/views/partial/header.ht',
+      'utf8'
+    );
+    res.write(header);
+
+    var data = fs.readFileSync(__dirname + '/views/homepage.html', 'utf8');
+    res.write(data);
+
+    var footer = fs.readFileSync(
+      __dirname + '/views/partial/footer.ht',
+      'utf8'
+    );
+    res.write(footer);
+
+    res.end();
+  }
+});
+
+server.listen(1000, '127.0.0.1');
 
 console.log(`Hello Node.js v${process.versions.node}!`);
