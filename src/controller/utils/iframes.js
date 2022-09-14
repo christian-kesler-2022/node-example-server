134918b5023ddc783fa04d5032d862d7b6eeaec0
christian-kesler-2022 <111981585+christian-kesler-2022@users.noreply.github.com>
2022-09-14 12:47:16 -0700

    Trim old code

--- src/controller/utils/iframes.js
+++ src/controller/utils/iframes.js
@@ -9,34 +9,14 @@ module.exports = {
         console.log('Unable to scan directory: ' + err);
         res.write('Unable to scan directory: ' + err);
       } else {
-        // var content = fs.readFileSync(
-        //   __dirname +
-        //     '/../../views/demos/xml-validator/xml-validator-iframe.html',
-        //   'utf8'
-        // );
-        // res.write(content);
-
         var fileArray = [];
-
-        // res.write("<a href=''>Refresh</a><br>");
-        // res.write('================<br>');
         files.forEach(function (file) {
           item = {
             name: file,
             link: '/download?dir=' + dir + '&file=' + file,
           };
           fileArray.push(item);
-          // res.write(
-          //   '\t<a href="/download?dir=' +
-          //     dir +
-          //     '&file=' +
-          //     file +
-          //     '">' +
-          //     file +
-          //     '</a><br>'
-          // );
         });
-        // res.write('================');
         res.write(JSON.stringify(fileArray));
       }
       res.end();
